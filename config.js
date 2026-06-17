30807e3a257f49827e11dff0b83acf28376c0d7e
xapex-art <xapex1999@gmail.com>
2026-06-17 21:47:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,32 @@
-// config.js
 module.exports = {
-    // WORK_TYPE : 'private', 'public', 'group', 'inbox'
-    WORK_TYPE: process.env.WORK_TYPE || 'public', 
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || false,
-    AUTO_TYPING: process.env.AUTO_TYPING || false,
-    AUTO_REACT: process.env.AUTO_REACT || false,
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || false,
-    PREFIX: process.env.PREFIX || '/',
-    REACT_EMOJIS: ['❤️', '👍', '😂', '🔥', '✨', '🌸', '✅']
+    PREFIX: ".",
+    OWNER_NUMBER: "947XXXXXXXX",
+    BOT_NAME: "DTEC MINI V3",
+    WORK_TYPE: "public",
+
+    AUTO_TYPING: false,
+    AUTO_RECORDING: true,
+    ALWAYS_ONLINE: false,
+    ALWAYS_OFFLINE: true,
+    AUTO_READ: false,
+    READ_CMD_ONLY: true,
+    AUTO_BIO: true,
+
+    AUTO_REACT: false,
+    REACT_EMOJIS: ['❤️','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+
+    AUTO_READ_STATUS: true,
+    AUTO_VIEW_STATUS: true,
+    AUTO_LIKE_STATUS: true,
+    AUTO_LIKE_EMOJI: ['❤️', '🔥', '👍', '😂', '😮'],
+
+    ANTI_BOT: false,
+    ANTI_BAD: false,
+    ANTI_LINK: false,
+    ANTI_DELETE: "true",
+
+    NEWSLETTER_JID: "",
+    GROUP_INVITE_LINK: "",
+    MAX_RETRIES: 3,
+    RCD_IMAGE_PATH: "https://i.ibb.co/d4s455Fn/f32b832e8b8d.jpg"
 };
